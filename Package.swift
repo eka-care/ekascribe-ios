8f81c6d4bca88145c446470dbfb2170fe85af378
Divyesh Jivani <divyesh.jivani@eka.care>
2026-03-26 19:41:59 +0530

    Dependency removed.

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/groue/GRDB.swift.git", branch: "master"),
         .package(url: "https://github.com/aws-amplify/aws-sdk-ios-spm.git", .upToNextMajor(from: "2.36.6")),
-        .package(url: "https://github.com/microsoft/onnxruntime-swift-package-manager.git", exact: "1.17.0"),
+//        .package(url: "https://github.com/microsoft/onnxruntime-swift-package-manager.git", exact: "1.17.0"),
         .package(url: "https://github.com/gfreezy/libfvad.git", branch: "main"),
         .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.10.2")),
         .package(url: "https://github.com/apple/swift-atomics.git", from: "1.2.0")
@@ -24,7 +24,7 @@ let package = Package(
                 .product(name: "GRDB", package: "GRDB.swift"),
                 .product(name: "AWSCore", package: "aws-sdk-ios-spm"),
                 .product(name: "AWSS3", package: "aws-sdk-ios-spm"),
-                .product(name: "onnxruntime", package: "onnxruntime-swift-package-manager"),
+//                .product(name: "onnxruntime", package: "onnxruntime-swift-package-manager"),
                 "libfvad",
                 "Alamofire",
                 .product(name: "Atomics", package: "swift-atomics")
